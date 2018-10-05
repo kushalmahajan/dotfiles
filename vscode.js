1e7f8805f5d989c26edaf9beaeb406e10f5e8874
Kushal V. Mahajan <11805936+kushalmahajan@users.noreply.github.com>
2018-10-05 18:39:59 +0530

    Update vscode.js
    Vuejs lint added

--- vscode.js
+++ vscode.js
@@ -1,54 +1,88 @@
 {
-  "atomKeymap.promptV3Features": true,
-  "editor.multiCursorModifier": "ctrlCmd",
-  "editor.formatOnPaste": true,
-  "eslint.packageManager": "yarn",
-  "workbench.editor.enablePreview": true,
-  "workbench.iconTheme": "vscode-icons",
-  "workbench.colorTheme": "Atom One Dark",
-  "editor.fontSize": 11.4,
-  "editor.tabSize": 4,
-  "editor.hover.enabled": true,
-  "files.exclude": {
-    "USE_GITIGNORE": true,
-    "node_modules": true,
-    "**/.log": true,
-    "**/.md": true,
-    "**/.lock": true,
-    ".history": true,
-    "**/.git": true,
-    "**/.svn": true,
-    "**/.hg": true,
-    "**/CVS": true,
-    "**/.DS_Store": true,
-    "**/.next": true
-  },
-  "editor.minimap.enabled": true,
-  "editor.cursorWidth": 3,
-  "editor.cursorBlinking": "solid",
-  "workbench.activityBar.visible": true,
-  "workbench.editor.enablePreviewFromQuickOpen": false,
-  "emmet.includeLanguages": {
-    "javascript": "javascriptreact"
-  },
-  "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
-  "search.exclude": {
-    "**/node_modules": true,
-    "**/bower_components": true,
-    ".history": true
-  },
-  "git.confirmSync": false,
-  "javascript.updateImportsOnFileMove.enabled": "always",
-  "terminal.integrated.rendererType": "dom",
-  "vsicons.dontShowNewVersionMessage": true,
-  "editor.formatOnSave": true,
-  "[javascript]": {
+    "atomKeymap.promptV3Features": true,
+    "terminal.integrated.shell.windows": "C:\\Users\\16890\\AppData\\Local\\Programs\\Git\\bin\\sh.exe",
+    "editor.multiCursorModifier": "ctrlCmd",
+    "eslint.packageManager": "yarn",
+    "workbench.editor.enablePreview": true,
+    // Indentation
+    "editor.tabSize": 4,
+    "editor.insertSpaces": true,
+    "editor.detectIndentation": false,
+    //Formatting
+    "editor.formatOnPaste": true,
+    //Excludes
+    "files.exclude": {
+        "USE_GITIGNORE": true,
+        "node_modules": true,
+        "**/.log": true,
+        "**/.md": true,
+        "**/.lock": true,
+        ".history": true,
+        "**/.git": true,
+        "**/.svn": true,
+        "**/.hg": true,
+        "**/CVS": true,
+        "**/.DS_Store": true,
+        "**/.next": true
+    },
+    "emmet.includeLanguages": {
+        "javascript": "javascriptreact"
+    },
+    "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+    "search.exclude": {
+        "**/node_modules": true,
+        "**/bower_components": true,
+        ".history": true
+    },
+    "git.confirmSync": false,
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "terminal.integrated.rendererType": "dom",
+    "vsicons.dontShowNewVersionMessage": true,
+    "editor.autoIndent": false,
+    "editor.trimAutoWhitespace": true,
+    "breadcrumbs.enabled": true,
+    "files.autoSave": "onWindowChange",
+    "git.enableSmartCommit": true,
+    "window.zoomLevel": 0,
+    // Workbench and editor Theming and window looks
+    "workbench.iconTheme": "vscode-icons",
+    "workbench.colorTheme": "Default Dark+",
+    "editor.hover.enabled": true,
+    "editor.minimap.enabled": true,
+    "editor.cursorWidth": 3,
+    "editor.cursorBlinking": "solid",
+    "workbench.startupEditor": "newUntitledFile",
+    "workbench.statusBar.feedback.visible": false,
+    "workbench.activityBar.visible": true,
+    "workbench.editor.enablePreviewFromQuickOpen": false,
     "editor.formatOnSave": false,
-  },
-  "eslint.autoFixOnSave": true,
-  "editor.autoIndent": false,
-  "editor.trimAutoWhitespace": true,
-  "eslint.alwaysShowStatus": true,
-  "breadcrumbs.enabled": true,
-  "workbench.startupEditor": "newUntitledFile"
+    "javascript.format.enable": false,
+    "eslint.autoFixOnSave": true,
+    "eslint.alwaysShowStatus": true,
+    "eslint.options": {
+        "extensions": [
+            ".html",
+            ".js",
+            ".vue",
+            ".jsx"
+        ]
+    },
+    "eslint.validate": [
+        {
+            "language": "html",
+            "autoFix": true
+        },
+        {
+            "language": "vue",
+            "autoFix": true
+        },
+        {
+            "language": "javascript",
+            "autoFix": true
+        },
+        {
+            "language": "javascriptreact",
+            "autoFix": true
+        }
+    ]
 }
